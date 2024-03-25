a4cb336811f2fece9ece72dd0a2344a4c8b98d88
ugurs <ugurs@UGURSEVILGEN>
2024-03-25 11:37:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Araba_Uretim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            List<Uretim> aracList = new List<Uretim>()
+            {
+                new Uretim("volkswagen","passat","siyah",2000,2008),
+                new Uretim("volkswagen","golf","kırmızı",1400,2017),
+                new Uretim("opel","corsa","siyah",1300,2013),
+                new Uretim("opel","astra","mavi",1600,2012),
+                new Uretim("toyota","corolla","beyaz",1400,2017),
+                new Uretim("toyota","yaris","beyaz",1500,2017),
+                new Uretim("bmw","320d","kahverengi",2000,2018),               
+                new Uretim("bmw","520d","mavi",2000,2018),               
+                new Uretim("hyundai","accent","gri",1500,1998),               
+                new Uretim("hyundai","i20","kırmızı",1400,2023),               
+          
+            };
+            while (true)
+            {
+                Console.WriteLine("1 - Kayıt\n2 - Liste\n3 - Güncelle\n4 - Filtre\n5 - Çıkış");
+               
+                int secim = Convert.ToInt32(Console.ReadLine());
+                if (secim == 1)
+                {
+                    Kayit(aracList);
+                }
+                else if (secim == 2)
+                {
+                    Listele(aracList);
+                }
+                else if (secim == 3)
+                {
+                    Guncelleme(aracList);
+                }
+                else if (secim == 4)
+                {
+                    Filtrele(aracList);
+                }
+                else if (secim == 5)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Hatalı Giriş Yaptınız.");
+                }
+            }
+        }
+        static void Kayit(List<Uretim> liste)
+        {
+            Console.WriteLine("Marka:");
+            string marka = Console.ReadLine().ToLower();
+
+            Console.WriteLine("Model:");
+            string model = Console.ReadLine().ToLower();
+
+            Console.WriteLine("Renk:");
+            string renk = Console.ReadLine().ToLower();
+
+            Console.WriteLine("Motor Hacmi:");
+            int motorHacmi = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Üretim Yılı:");
+            int uretimYili = Convert.ToInt32(Console.ReadLine());
+
+            Uretim uretim = new Uretim(marka, model, renk, motorHacmi, uretimYili);
+            liste.Add(uretim);
+        }
+        static void Listele(List<Uretim> liste)
+        {
+            Uretim.Goster(liste);
+        }
+        static void Guncelleme(List<Uretim> liste)
+        {
+            Listele(liste);
+            Console.WriteLine("Güncellenecek araç numarası:");
+            int numara = Convert.ToInt32(Console.ReadLine());
+            Uretim.Guncelleme(liste, numara);
+        }
+        static void Filtrele(List<Uretim> liste)
+        {
+            Console.WriteLine("Filtrelenecek marka:");
+            string marka1 = Console.ReadLine().ToLower();
+            Uretim.Filtrele(liste, marka1);
+        }
+    }
+}
---
+++ Uretim.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Araba_Uretim
+{
+    internal class Uretim
+    {
+        string marka;
+        string model;
+        string renk;
+        int motorHacmi;
+        int uretimYili;
+
+        public Uretim(string marka, string model, string renk, int motorHacmi, int uretimYili)
+        {
+            this.marka = marka;
+            this.model = model;
+            this.renk = renk;
+            this.motorHacmi = motorHacmi;
+            this.uretimYili = uretimYili;
+        }
+        public static void Goster(List<Uretim> liste)
+        {
+            int i = 1;
+            Console.WriteLine("Arabalar Listesi: \n\n");
+            foreach (Uretim item in liste)
+            {
+                Console.WriteLine(i + " -Araba Marka: " + item.marka + "\nAraba Modeli: " + item.model + "\nRengi: " + item.renk + "\nMotor Hacmi: " + item.motorHacmi + "\nÜretim Yılı: " + item.uretimYili +"\n");
+
+                i++;
+            }
+        }
+        public static void Guncelleme(List<Uretim> liste, int i)
+        {
+            Console.WriteLine("Güncelleme Türünü Seç:1-Renk\n2-Motor Hacmi");
+            int secim = Convert.ToInt32(Console.ReadLine());
+            if (secim==1)
+            {
+                Console.WriteLine("Arabanın Rengi: " + liste[i - 1].renk + "\nGüncellemek istediğin renk ?");
+                string yeniRenk = Console.ReadLine();
+                liste[i - 1].renk = yeniRenk;
+            }
+            else if (secim==2)
+            {
+                Console.WriteLine("Arabanın Motor Hacmi: " + liste[i - 1].motorHacmi + "\nGüncellemek istediğin motor hacmi ?");
+                int yeniMotor = Convert.ToInt32(Console.ReadLine());
+                liste[i - 1].motorHacmi = yeniMotor;
+            }
+            
+        }
+        public static void Filtrele(List<Uretim> liste, string marka1)
+        {
+            for (int i = 0; i < liste.Count; i++)
+            {
+                if (liste[i].marka == marka1)
+                {
+                    Console.WriteLine((i + 1) + "Araba Marka: " + liste[i].marka + "\nAraba Modeli: " + liste[i].model + "\nRengi: " + liste[i].renk + "\nMotor Hacmi: " + liste[i].motorHacmi + "\nÜretim Yılı: " + liste[i].uretimYili);
+
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
